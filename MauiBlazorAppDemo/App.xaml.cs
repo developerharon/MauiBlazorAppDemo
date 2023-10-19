3f1a795945078454535b0b3aac98af183ad815c3
Haron Kipkorir <ronoharonkip@gmail.com>
2023-10-19 11:44:05 +0300

    Add project files.

---
+++ MauiBlazorAppDemo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiBlazorAppDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MauiBlazorAppDemo/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBlazorAppDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
