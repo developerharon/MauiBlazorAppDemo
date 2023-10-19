8c7f11861e2c8334136626cd0a8cac27e9c9efe6
Haron Kipkorir <ronoharonkip@gmail.com>
2023-10-19 16:10:36 +0300

    Updated it to the default mudblazor templated and added a login form

---
+++ MauiBlazorAppDemo/Data/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MauiBlazorAppDemo.Data
+{
+    public class LoginModel
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ MauiBlazorAppDemo/Pages/LoginPage.razor.cs
@@ -0,0 +1,37 @@
+﻿using MauiBlazorAppDemo.Data;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace MauiBlazorAppDemo.Pages
+{
+    public partial class LoginPage : ComponentBase
+    {
+        private bool passwordVisibility;
+        private InputType passwordInputType = InputType.Password;
+        private string passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+
+        private LoginModel loginModel = new LoginModel();
+        private string message = "";
+
+        private void TogglePasswordVisibility()
+        {
+            if (passwordVisibility)
+            {
+                passwordVisibility = false;
+                passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+                passwordInputType = InputType.Password;
+            }
+            else
+            {
+                passwordVisibility = true;
+                passwordInputIcon = Icons.Material.Filled.Visibility;
+                passwordInputType = InputType.Text;
+            }
+        }
+
+        public void LoginUserAsync()
+        {
+
+        }
+    }
+}
